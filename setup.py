3da2f2afab152da87c2b461fed9f9621fc161779
Fernando <fbayon@gmail.com>
2021-10-28 00:20:10 +0200

    Revert "Update setup.py"
    This reverts commit 1486e085d4d7878ac3f4ebcf7bd245eb13d398ab.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='passivbot_manager',
+    name='passivbot-manager',
     packages=find_packages(),
     version='0.1.0',
     description='Passivbot Manager Library',
