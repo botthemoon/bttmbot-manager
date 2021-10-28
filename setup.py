2789ad85d19807a38af6fd5b2e1b9ddc464a793a
Miguel Tejedor <m.tejedor@medlabmg.com>
2021-10-28 22:48:40 +0200

    fix package install

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import find_packages, setup
 
 with open("requirements.txt") as f:
     requires = f.read().splitlines()
-    requires = requires[:len(requires) - 2]
+    requires = requires[: len(requires) - 2]
 
 with open("passivbot/requirements_liveonly.txt") as f:
     requires_passivbot = f.read().splitlines()
-    requires = requires.extend(requires_passivbot)
+    requires.extend(requires_passivbot)
 
 setup(
     name="passivbot_manager",
-    version="0.1.2",
+    version="0.1.3",
     description="Passivbot Manager Library",
     author="BTM",
     url="https://github.com/botthemoon/passivbot-manager",
