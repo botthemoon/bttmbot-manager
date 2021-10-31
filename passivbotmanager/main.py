c6c8367176d23d4c8aa8de3a5699688afebcde27
Miguel Tejedor <m.tejedor@medlabmg.com>
2021-10-31 10:56:23 +0100

    Update passivbot, multiconfig symbol

--- passivbotmanager/main.py
+++ passivbotmanager/main.py
@@ -15,9 +15,8 @@ def run_passivbot(
     env_bot = {}
     env_bot["PASSIVBOT_MARKET_TYPE"] = market_type
     env_bot["PASSIVBOT_SYMBOL"] = symbol.upper()
-    env_bot[
-        "PASSIVBOT_CONFIG_PATH"
-    ] = f"passivbot/configs/live/binance_{symbol.lower()}.json"
+    # env_bot["PASSIVBOT_CONFIG_PATH"] = f"passivbot/configs/live/binance_{symbol.lower()}.json"
+    env_bot["PASSIVBOT_CONFIG_PATH"] = "passivbot/configs/live/binance_usdt_84_symbols_2021-05-01_to_2021-10-20_mean_adg_0.0335%_mean_PAc_3.97%"
     env_bot["PASSIVBOT_BINANCE_KEY"] = userbot_key
     env_bot["PASSIVBOT_BINANCE_SECRET"] = userbot_secret
     return run_bot_container(env=env_bot, name=userbot_name)
